8e8c2bb754e7eeb117e996a5a603e98045b76c32
lotva <dlotva@gmail.com>
2026-05-15 20:07:03 +0300

    test: increase graph size

--- tasks/nalitov_d_dijkstras_algorithm/tests/performance/main.cpp
+++ tasks/nalitov_d_dijkstras_algorithm/tests/performance/main.cpp
@@ -29,7 +29,7 @@ InType MakeStarFromZero(int n) {
 }  // namespace
 
 class NalitovDDijkstrasAlgorithmPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  static constexpr int kGraphSize = 150;
+  static constexpr int kGraphSize = 10000;
   InType input_data_{};
   OutType expected_output_{};
 
